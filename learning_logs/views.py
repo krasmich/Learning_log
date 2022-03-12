038e636edd2082364926c3f46c8f325e5494ce54
krasmich <kraskovmichael@gmail.com>
2022-03-12 23:28:57 +0400

    topic

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -3,6 +3,7 @@ from .models import Topic
 
 from learning_logs.models import Topic
 
+
 def index(request):
     """Домашняя страница приложения Learning Log"""
     return render(request, 'learning_logs/index.html')
@@ -12,3 +13,11 @@ def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
+
+
+def topic(request, topic_id):
+    """Выводит одну тему и все ее записи"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
